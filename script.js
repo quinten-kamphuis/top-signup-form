339d99832883813a601cf739bc68c64a59d0c9dc
Quinten Kamphuis <163667554+quinten-kamphuis@users.noreply.github.com>
2024-05-22 06:47:43 +0200

    Add form validation logic

--- script.js
+++ script.js
@@ -5,8 +5,25 @@ const checkPasswords = () => {
     const confirmPassword = inputs.find(input => input.id === 'confirm-password')
     if (password.value === confirmPassword.value) return true;
     return false;
-}
+};
 
-document.querySelector('.btn').addEventListener('click', () => {
-    console.log(checkPasswords());
-});
+const checkFormInputs = () => {
+    inputs.forEach(elem => elem.parentNode.classList.add('invalid'))
+    inputs.forEach(elem => {
+        if (elem.value !== ''){
+            elem.parentNode.classList.remove('invalid')
+        }
+    })
+    inputs.forEach(elem => {
+        if (elem.id.includes('password') && !checkPasswords()){
+            elem.parentNode.classList.add('invalid')
+
+        }
+        if (elem.id.includes('password') && checkPasswords()){
+            elem.parentNode.classList.remove('invalid')
+        }
+    })
+};
+
+
+document.querySelector('.btn').addEventListener('click', () => checkFormInputs());
